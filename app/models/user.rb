0b5891f14535241ff61ce36034b339ed5b43258c
Shane Krusen <shanekrusen@gmail.com>
2017-07-03 20:20:39 -0400

    Added controllers, models, routes, and methods for user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ApplicationRecord
+    attr_accessor :password
+    validates_presence_of :first_name, :last_name, :email, :password
+    validates :password, :confirmation => :true
+    validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+    before_save :encrypt_password
+    before_create { generate_token(:auth_token) }
+    after_save :clear_password
+
+    def encrypt_password
+        if password.present?
+            self.salt = BCrypt::Engine.generate_salt
+            self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)
+        end
+    end
+
+    def clear_password
+        self.password = nil
+    end
+
+    def self.authenticate(login_email="", login_password="")
+        user = User.find_by_email(login_email)
+        if user && user.match_password(login_password)
+            return user
+        else
+            return false
+        end
+    end
+
+    def match_password(login_password="")
+        encrypted_password == BCrypt::Engine.hash_secret(login_password, salt)
+    end
+
+    def generate_token(column)
+        begin
+            self[column] = SecureRandom.urlsafe_base64            
+        end while User.exists?(column => self[column])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/login'
+
+  get 'log_out' => 'sessions#destroy', :as => 'log_out'
+
+  post 'sessions/login_attempt'
+
+  get 'sessions/home'
+
+  get 'sessions/profile'
+
+  get 'sessions/setting'
+
+  get 'users/new'
+
+  root :to => 'statics#home'
+
+  resources :users
 end
