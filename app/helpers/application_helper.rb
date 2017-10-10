d48ce89787c78daca970e3cfcb844d46429056a9
Shane Krusen <shanekrusen@gmail.com>
2017-10-09 20:39:54 -0400

    UNFINISHED added test for find nearby users method, proved it does not work, may need to increase range

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-    def find_nearby_users
-        @low_lat = @current_user.lat + (-32 / 6378) * (180 / Math::PI)
-        @high_lat = @current_user.lat + (32 / 6378) * (180 / Math::PI)
-        @low_long = @current_user.long + (-32 / 6378) * (180 / Math::PI) / Math.cos(@current_user.lat * Math::PI / 180)
-        @high_long = @current_user.long + (32 / 6378) * (180 / Math::PI) / Math.cos(@current_user.lat * Math::PI / 180)
+    def find_nearby_users(user)
+      @low_lat = user.lat + (-32 / 6378) * (180 / Math::PI)
+      @high_lat = user.lat + (32 / 6378) * (180 / Math::PI)
+      @low_long = user.long + (-32 / 6378) * (180 / Math::PI) / Math.cos(user.lat * Math::PI / 180)
+      @high_long = user.long + (32 / 6378) * (180 / Math::PI) / Math.cos(user.lat * Math::PI / 180)
 
-        @nearby_user_lat = User.where(lat: @low_lat..@high_lat)
-        @nearby_user_long = User.where(long: @low_long..@high_long)
+      @nearby_user_lat = User.where(lat: @low_lat..@high_lat)
+      @nearby_user_long = User.where(long: @low_long..@high_long)
 
-        @nearby_users = @nearby_user_long & @nearby_user_lat
+      @nearby_users = @nearby_user_long & @nearby_user_lat
 
-        return @nearby_users
+      return @nearby_users
     end
 end
