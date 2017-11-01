483311c066cf4eeadde011e9398dbadb13910bb5
Shane Krusen <shanekrusen@gmail.com>
2017-11-01 10:09:00 -0400

    Changed user object to have two different types, volunteer or in need

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810234038) do
+ActiveRecord::Schema.define(version: 20171031234533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170810234038) do
     t.string   "auth_token"
     t.float    "lat"
     t.float    "long"
+    t.boolean  "volunteer"
     t.index ["lat", "long"], name: "index_users_on_lat_and_long", using: :btree
     t.index ["lat"], name: "index_users_on_lat", using: :btree
     t.index ["long"], name: "index_users_on_long", using: :btree
