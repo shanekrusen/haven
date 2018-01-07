8038d8615a1d36324f4944cb66e39b44f248b9c2
Shane Krusen <shanekrusen@gmail.com>
2018-01-06 19:39:35 -0500

    Fixed for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_cable.allowed_request_origins = ['https://becomehaven.herokuapp.com', 'http://becomehave.herokuapp.com']
+  config.action_cable.url = "wss://becomehaven.herokuapp.com/cable"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
